ef0b6042e6ebb9d18c6949ff1d47e59f2bd4b030
AAYUSH\Aayush Bhandari <aayuk.93@gmail.com>
2017-11-30 22:47:11 -0600

    Final version

--- KMeansClustering.py
+++ KMeansClustering.py
@@ -1,8 +1,10 @@
-import numpy as np
+import numpy as np,sys
 import copy
 from sklearn.metrics.pairwise import euclidean_distances as ed
 import random
-input_file = open("test_data.txt","r")
+#input_file = open("test_data.txt","r")
+input_file = open(sys.argv[2],"r")
+
 #print(input_file.read())
 
 cordinates = {}
@@ -21,7 +23,7 @@ def kMeans(k,means):
     for x in range(k):
         id = random.randint(1, len(cordinates))
         means[int(id)] = cordinates[id]
-    print(means)
+    #print(means)
     return means;
 
 
@@ -32,9 +34,7 @@ def predictMean(cordinates, means,predictedCategory):
         minDistance = 10
         tempMean = 0
         for mean in means:
-            #print("point is ", id, "mean is", mean)
             distance = (ed([cordinates[id]], [means[mean]]))
-            #print(distance[0][0])
             if(distance[0][0]<minDistance):
                 minDistance = distance[0][0]
                 tempMean = mean
@@ -43,10 +43,6 @@ def predictMean(cordinates, means,predictedCategory):
             predictedCategory[tempMean] = [id]
         else:
             predictedCategory[tempMean].append(id)
-        #print("closer to ", tempMean)
-        #print()
-
-    #print(predictedCategory)
     return predictedCategory
 
 # Recompute values of means
@@ -62,7 +58,6 @@ def recomputeMeans(predictedCategory, means):
         newY = round(newY/len(listOfPoints),2)
         means[eachMean] = [newX, newY]
     return means
-    #print(means)
 
 #Calculate sum squared error
 def caculateSSE(means,predictedCategory,cordinates):
@@ -72,30 +67,33 @@ def caculateSSE(means,predictedCategory,cordinates):
         for id in list_points:
             sse = sse + ed([cordinates[id]], [means[p]])
     print("SSE = ",sse[0][0])
+    return sse[0][0]
 
-#calulate Mean
+
 #Main method
 def main():
     cordinates = preProcess();
-    k = 5;
+    #k = 5;
+    k = int(sys.argv[1])
     means = {}
     means = kMeans(k,means)
     prevMeans = {}
-   #Iterate over predecting category and computing means till we converge or we reach max iterations
+    #Iterate over predecting category and computing means till we converge or we reach max iterations
     for i in range(0,25):
         predictedCategory = {}
         predictedCategory = predictMean(cordinates,means,predictedCategory)
         prevMeans = copy.deepcopy(means)
         means = recomputeMeans(predictedCategory,means)
-        print("prevMean", prevMeans)
-        print("means", means)
-        print("Iteration No", i)
-        for mean in predictedCategory:
-            print(mean, len(predictedCategory[mean]))
+        # If there are no changes in means cordinates, terminate the loop
         if (prevMeans == means):
             break
     print("Final Category",predictedCategory)
 
-    caculateSSE(means,predictedCategory,cordinates)
+    #outputfile = "out.txt"
+    outputfile = sys.argv[3]
+    with open(outputfile, 'w') as outfile:
+        for mean in predictedCategory:
+                outfile.write( (str(mean)+":"+str(predictedCategory[mean]))+'\n')
+        outfile.write('\n'+"SSE:"+ str(caculateSSE(means,predictedCategory,cordinates)))
 
 main()
